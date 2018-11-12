d68746fb4eb3b6bd9a5d51610b1bacfdea220069
John O'Sullivan <jnosullivan@icloud.com>
2018-11-11 19:00:59 -0600

    Cleaning Up

--- src/App.js
+++ src/App.js
@@ -3,36 +3,16 @@ import DMHeader from './Header';
 import DMBody from './Body';
 
 import { Connect } from 'uport-connect';
-//
-//import logo from './logo.svg';
-//import Footer from './Footer';
-
-/*
-import {
-  Button,
-  Icon,
-  Modal,
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Input
-} from 'semantic-ui-react';
-*/
 
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
 
   constructor() {
     super();
-    console.log(window.web3);
+    console.log(window.ethereum);
+
+    window.ethereum.enable();
 
     /*
     const uport = new Connect('dMarket', {network: 'ropsten'});
