83f1da1351af013b043620af18aad000b147c67c
John O'Sullivan <jnosullivan@icloud.com>
2018-11-19 23:34:14 -0600

    auth flow nav bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Body from './Body';
 
-//import { Connect } from 'uport-connect';
+import { Connect } from 'uport-connect';
 
 import './App.css';
 
--- src/Header.js
+++ src/Header.js
@@ -2,37 +2,65 @@ import React from 'react';
 import { 
   Navbar,
   Nav,
-  Form,
-  FormControl,
   Button,
-  bootstrapUtils
+  Grid,
+  Row,
+  Col,
+  Image,
+  Dropdown,
+  MenuItem
 } from 'react-bootstrap';
 
 import uport from './uport.svg';
 
-const Header = () => (
-  <div>
-    <style type="text/css">
-      {`
-    .btn-custom {
-        background-color: rgb(92, 80, 202);
-        color: white;
-    }
-    `}
-    </style>
-    <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="">dMarket</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="">Home</Nav.Link>
-        <Nav.Link href="">About</Nav.Link>
-      </Nav>
-      <Button variant="custom">
-        <img src={uport}/>
-      </Button>
-    </Navbar.Collapse>
-    </Navbar>
-  </div>
-)
-export default Header;
+export default class Header extends React.Component {
+
+  state = {
+    isAuth: false
+  }
+
+  constructor() {
+    super();
+  }
+
+  login = () => {
+    this.setState({ isAuth: true });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="">dMarket</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="">Home</Nav.Link>
+              <Nav.Link href="">About</Nav.Link>
+            </Nav>
+            { !this.state.isAuth &&
+              <div>
+                <Button variant="custom" onClick={this.login}> 
+                  <img src={uport}/>
+                </Button>
+              </div>
+            }
+            { this.state.isAuth &&
+              <div>
+                <Row>
+                  <Col>
+                    <img src="https://ipfs.infura.io/ipfs/QmcCCfuXWswSKt7ULnF19ATuoUt1cC5yUNgf4LFL8pQyTq" style={{
+                      width: 40,
+                      height: 40,
+                      borderRadius: 20
+                    }}/>
+                  </Col>
+                </Row>
+              </div>
+            }
+          </Navbar.Collapse>
+        </Navbar>
+    </div>
+    );
+  }
+}
