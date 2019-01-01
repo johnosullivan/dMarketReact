4d749171480e4ec442734eb18559a6cb6b010f06
John O'Sullivan <jnosullivan@icloud.com>
2019-01-01 11:15:11 +0100

    added functions to provider

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -1,5 +1,49 @@
-const dataProvider = {};
+const DataProvider = {};
 
+const web3 = new window.Web3(window.web3.currentProvider);
 
+DataProvider.bytesToHex = (bytes) => {
+    for (var hex = [], i = 0; i < bytes.length; i++) {
+        hex.push((bytes[i] >>> 4).toString(16));
+        hex.push((bytes[i] & 0xF).toString(16));
+    }
+    return hex.join("");
+}
 
-module.exports = dataProvider;
+DataProvider.hexToBytes = (hex) => {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
+
+DataProvider.getMyFilesCount = async () => {
+    const self = this;
+    return new Promise(function(resolve, reject) {
+      self.fileManager.getMyFilesCount(function(err, data) { 
+        if (err) { reject(err); } else { resolve(data['c'][0]); }
+      });
+    });
+};
+
+DataProvider.getMyFilesAt = async (index) => {
+    const self = this;
+    return new Promise(function(resolve, reject) {
+      self.fileManager.getMyFilesAt(index, function(err, data) { 
+        if (err) { reject(err); } else { resolve(data); }
+      });
+    });
+};
+
+DataProvider.getFilePublicDetails = async (address) => {
+    const self = this;
+    return new Promise(function(resolve, reject) {
+      self.file.at(address).getPublicDetails(function(err, data) { 
+        if (err) { reject(err); } else { resolve(data); }
+      });
+    });
+};
+
+module.exports = {
+    DataProvider,
+    web3
+};
