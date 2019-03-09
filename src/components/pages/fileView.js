b3742a96baa560ee0796833867ec55be30ae3ad5
John O'Sullivan <jnosullivan@icloud.com>
2019-03-08 22:41:41 -0600

    React fix

--- src/components/pages/fileView.js
+++ src/components/pages/fileView.js
@@ -14,7 +14,9 @@ import {
     Grid
  } from '@material-ui/core';
 
- import { Done } from '@material-ui/icons';
+import { Done } from '@material-ui/icons';
+
+const deplay = ms => new Promise(res => setTimeout(res, ms))
 
 class FileView extends React.Component {
 
@@ -23,11 +25,12 @@ class FileView extends React.Component {
         address: '',
         cost: 0,
         isLoading: false,
-        alertOpened: true,
+        alertOpened: false,
         approveHash: '',
         approveDone: false,
         buyHash: '',
-        buyDone: false
+        buyDone: false,
+        waiting: false
     }
 
     constructor(props) {
@@ -46,6 +49,7 @@ class FileView extends React.Component {
     checkFileStatus = async (address) => {
         // Checks the access status of the metamask address
         const status = await this.dataProvider.getAccessStatus(address);
+        console.log('status:', status);
         if (status) {
             // Grabs the fileData
             const fileData = await this.dataProvider.getData(address);
@@ -57,9 +61,13 @@ class FileView extends React.Component {
         }
     }
 
+    buyAction = () => {
+        this.setState({ alertOpened: true });
+    };
+
     buy = async () => {
         const { hasAccess, address, cost } = this.state;
-        //if (!hasAccess) {
+        if (!hasAccess) {
             try {
                 // Creates the approve for the token transfer
                 const hashApprove = await this.dataProvider.approve(address, cost);
@@ -74,11 +82,15 @@ class FileView extends React.Component {
                 // Waits for the transaction to the completed
                 const receiptBuy = await this.dataProvider.transactionReceipt(hashBuy);
                 // Stops the animation of the spinner
-                this.setState({ isLoading: false, buyDone: false });
+                this.setState({ isLoading: false, buyDone: true, waiting: true });
+                // Start timer to update
+                await deplay(10000);
+                this.setState({ isLoading: false, alertOpened: false, waiting: false });
+                this.checkFileStatus(address);
             } catch (e) {
                 console.error(e);
             }
-       //}
+       }
     }
 
     handleClose = () => {
@@ -86,7 +98,7 @@ class FileView extends React.Component {
     };
 
     render() {
-        const { hasAccess, isLoading, alertOpened, address, approveHash, approveDone, buyHash, buyDone } = this.state;
+        const { hasAccess, isLoading, alertOpened, address, approveHash, approveDone, buyHash, buyDone, waiting } = this.state;
 
 
         const classes = {
@@ -102,69 +114,72 @@ class FileView extends React.Component {
         if (hasAccess) {
             return (
                 <div>
-                    FileView View
+                    
                 </div>
             );
         } else {
             return (
                 <div>
                     <br/>
-                    <Button variant="contained" color="secondary" onClick={this.buy}>
-                        Buy
-                    </Button>
+                    <Button variant="contained" color="secondary" onClick={this.buyAction}>Buy</Button>
                     <br/>
                     <Dialog
-          open={alertOpened}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{"Purchase: Are you sure? "}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-                Are you sure you would like to purchase file? If not please cancel this operation below. If so, please select "Buy" below. To have a successful file purchase there will be two transactions. The first one is an approval for the file smart contract to withdrawl funding from your account (like a hold on a credit card). The next transaction will transfer the funds and enable access to the requested file.   
-            </DialogContentText>
-            <br/>
+                        open={alertOpened}
+                        onClose={this.handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title">{"Purchase: Are you sure? "}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Are you sure you would like to purchase file? If not please cancel this operation below. If so, please select "Buy" below. To have a successful file purchase there will be two transactions. The first one is an approval for the file smart contract to withdrawl funding from your account (like a hold on a credit card). The next transaction will transfer the funds and enable access to the requested file.   
+                        </DialogContentText>
+                        <br/>
 
-            <div className={classes.root}>
-                <Grid container spacing={24}>
-                    {approveHash != '' && 
-                        <Grid item xs={11}>
-                            <Typography color="textSecondary">
-                                <a href="">{approveHash}</a>
-                            </Typography>
-                        </Grid>}
-                    {approveHash != '' &&
-                        <Grid item xs={1}>
-                            {approveDone == true && <Done style={{ color: '#a0db8e' }}/>}
-                            {approveDone == false && <CircularProgress size={14}/>}
-                        </Grid>}
-                    {buyHash != '' &&
-                        <Grid item xs={11}>
-                            <Typography color="textSecondary">
-                                <a href="">{buyHash}</a>
-                            </Typography>
-                        </Grid>}
-                    {buyHash != '' &&
-                        <Grid item xs={1}>
-                            {buyDone == true && <Done style={{ color: '#a0db8e' }}/>}
-                            {buyDone == false && <CircularProgress size={14}/>}
-                        </Grid>}
-                </Grid>
-            </div>
-
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-                Cancel
-            </Button>
-            <Button onClick={this.buy} color="primary">
-                Buy
-            </Button>
-          </DialogActions>
-        </Dialog>
+                        <div className={classes.root}>
+                            <Grid container spacing={24}>
+                                {approveHash != '' && 
+                                    <Grid item xs={11}>
+                                        <Typography color="textSecondary">
+                                            <a href="">{approveHash}</a>
+                                        </Typography>
+                                    </Grid>}
+                                {approveHash != '' &&
+                                    <Grid item xs={1}>
+                                        {approveDone == true && <Done style={{ color: '#a0db8e' }}/>}
+                                        {approveDone == false && <CircularProgress size={14}/>}
+                                    </Grid>}
+                                {buyHash != '' &&
+                                    <Grid item xs={11}>
+                                        <Typography color="textSecondary">
+                                            <a href="">{buyHash}</a>
+                                        </Typography>
+                                    </Grid>}
+                                {buyHash != '' &&
+                                    <Grid item xs={1}>
+                                        {buyDone == true && <Done style={{ color: '#a0db8e' }}/>}
+                                        {buyDone == false && <CircularProgress size={14}/>}
+                                    </Grid>}
+                                {waiting && <Grid item xs={11}>
+                                        <Typography color="textSecondary">
+                                            Waiting for blockchain to reflect the changes...  
+                                        </Typography>
+                                    </Grid>}
+                                {waiting && <Grid item xs={1}>
+                                        <CircularProgress size={14}/>
+                                    </Grid>}
+                            </Grid>
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.buy} color="primary">
+                            Buy
+                        </Button>
+                    </DialogActions>
+                    </Dialog>
                     <br/>
-                    {isLoading && <CircularProgress size={24}/>}
                 </div>
             );
         }
