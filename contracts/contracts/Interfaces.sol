87d5d3e6e4f3e092ad9a57aace5ae53438de2a60
John O'Sullivan <jnosullivan@icloud.com>
2019-06-02 20:19:44 -0500

    added to truffle

---
+++ contracts/contracts/Interfaces.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+interface FileManagerInterface {
+    function addFile(address value) external;
+    function removeFile(address value) external;
+    function addMyFile(address value, address sender) external;
+    function addBoughtFile(address value, address buyer) external;
+}
+
+interface TokenInterface {
+    function transferFrom(address _from, address _to, uint256 _value) external returns(bool success);
+}
+
+interface FileContractManagerInterface {
+    function getContractAddress(bytes32 name) external returns (address);
+}
+
+interface tokenRecipient { 
+    function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; 
+}
--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const FileContractManager = artifacts.require("../contracts/FileContractManager.sol");
+const dPublishToken = artifacts.require("../contracts/dPublishToken.sol");
 
 module.exports = function(deployer) {
 	deployer.deploy(FileContractManager);
+	deployer.deploy(dPublishToken, 500000000000);
 };
